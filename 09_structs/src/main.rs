ac16cf0e2fbdbb5f729a5fffff1bf137324bb283
Francois Malassenet <malassenet@gmail.com>
2023-05-12 21:13:49 -0400

    09_struct: Comparing reference vs copy extrema and print type. (about the same time)

--- 09_structs/src/main.rs
+++ 09_structs/src/main.rs
@@ -1,4 +1,9 @@
-use std::cmp::{PartialOrd, Ord};
+use core::cell::Cell;
+use std::cmp::PartialOrd;
+use std::rc::Rc;
+use std::time::Instant;
+
+
 
 #[derive(Copy, Clone, Debug)]
 pub enum BroomIntent {
@@ -13,6 +18,11 @@ pub struct Broom {
     pub health: u32,
     pub position: (f32, f32, f32),
     pub intent: BroomIntent,
+}
+
+impl Broom {
+    const CENTER: (f32, f32, f32) = (0.0, 0.0, 0.0);
+    const NAME: &'static str = "BROOM";
 }
 
 fn chob(b: Broom) -> (Broom, Broom) {
@@ -75,6 +85,9 @@ impl<T> Queue<T> {
     }
 }
 
+fn type_of<T>(_: &T) -> &'static str {
+    std::any::type_name::<T>()
+}
 /// References to greatest and least elements
 #[derive(Debug)]
 pub struct ExtremaRef<'elt, T> {
@@ -83,8 +96,9 @@ pub struct ExtremaRef<'elt, T> {
 }
 
 /// Find the greatest and least numbers in a slice.
-pub fn find_extremas<'s, T>(slice: &'s [T]) -> ExtremaRef<'s, T>
-where T: PartialOrd,
+pub fn find_extrema_ref<'s, T>(slice: &'s [T]) -> ExtremaRef<'s, T>
+where
+    T: PartialOrd,
 {
     let mut greatest = &slice[0];
     let mut least = &slice[0];
@@ -99,15 +113,76 @@ where T: PartialOrd,
     ExtremaRef::<T> { greatest, least }
 }
 
+#[derive(Debug)]
+pub struct Extrema<T> {
+    pub greatest: T,
+    pub least: T,
+}
+
+/// Find the greatest and least numbers in a slice.
+pub fn find_extrema<T>(slice: &[T]) -> Extrema<T>
+where
+    T: PartialOrd + Copy,
+{
+    let mut greatest = slice[0];
+    let mut least = slice[0];
+    for i in 1..slice.len() {
+        if slice[i] < least {
+            least = slice[i];
+        }
+        if slice[i] > greatest {
+            greatest = slice[i];
+        }
+    }
+    Extrema::<T> { greatest, least }
+}
+
+/// Polynomial to have constant parameters
+
+#[derive(Copy, Clone, Debug)]
+pub struct Polynomial<const N: usize> {
+    coeffs: [f64; N],
+}
+
+impl<const N: usize> Polynomial<N> {
+    pub fn new(coeffs: [f64; N]) -> Self {
+        Self { coeffs }
+    }
+
+    pub fn eval(&self, x: f64) -> f64 {
+        let mut sum = 0.0;
+        for i in 0..N {
+            sum += self.coeffs[i] * x.powi(i as i32);
+        }
+        sum
+    }
+}
+
+#[derive(Clone, Debug)]
+// Everything will be immutable
+pub struct Robot {
+    pub name: String,
+    pub power_level: u32,
+    pub hardware_error_count: Cell<u32>, // allows to mutate the value:w
+}
+
+#[derive(Clone, Debug)]
+pub struct RobotSensor {
+    pub robot: Rc<Robot>,
+    pub range: f64,
+    pub angle: f64,
+}
+
 fn main() {
     let b = Broom {
-        name: "Zero".to_string(),
+        name: Broom::NAME.to_string(),
         height: 60,
         health: 100,
         position: (100.0, 200.0, 0.0),
         intent: BroomIntent::FetchWater,
     };
     println!("b is {:?}", b);
+    println!("CENTER: {:?}", Broom::CENTER);
 
     let (b1, b2) = chob(b);
     println!("b has been consumed");
@@ -157,21 +232,70 @@ fn main() {
     bq.push(1);
     println!("bq.is_empty() is {:?}", bq.is_empty());
 
-    println!("--------------");
-    println!("-- Extremas --");
-    println!("--------------");
-    let a = vec![0, 1, 10, -10, 100];
-    let extrema = find_extremas(&a);
+    println!("----------------");
+    println!("-- Polynomial --");
+    println!("----------------");
+    let a = [0., 1., 10.];
+    let p = Polynomial::new(a);
     // extrema.least = &100;
-    println!("extrema is {:p}", extrema.least);
+    println!("polynomial is {:?}", p);
+    let x = 2.;
+    println!("p({}) = {}", x, p.eval(x));
+
+    println!("---------------------------------");
+    println!("-- Testing Cell With Immutable --");
+    println!("---------------------------------");
+    let r = Robot {
+        name: "Bender".to_string(),
+        power_level: 100,
+        hardware_error_count: Cell::new(0),
+    };
+    println!("r is {:?}", r);
+    println!(" adding a hardware error even when r is immutable");
+    r.hardware_error_count.set(1);
+    println!("r is {:?}", r);
+
+    println!("-----------------");
+    println!("-- Extrema Ref --");
+    println!("-----------------");
+    let a: Vec<i64> = (0..600_000_000).map(|x| x as i64).collect();
+    let start = Instant::now();
+    let extrema_ref = find_extrema_ref(&a);
+    let elapsed = start.elapsed();
+    println!("extrema: {:?}", type_of(&a));
+    println!("extrema_ref calculation: {:?}", elapsed);
+    println!("extrema_ref is {:p}", extrema_ref.least);
+    println!("extrema_ref is {:?}, {:p}", extrema_ref, &extrema_ref);
+
+    println!("----------------");
+    println!("-- With float --");
+    println!("----------------");
+    let a_f: Vec<f64> = (0..600_000_000).map(|x| x as f64).collect();
+    let start = Instant::now();
+    let extrema_ref = find_extrema_ref(&a_f);
+    let elapsed = start.elapsed();
+    println!("extrema: {:?}", type_of(&a_f));
+    println!("extrema_ref calculation: {:?}", elapsed);
+    println!("extrema_ref is {:p}", extrema_ref.least);
+    println!("extrema_ref is {:?}, {:p}", extrema_ref, &extrema_ref);
+
+    println!("-------------");
+    println!("-- Extrema --");
+    println!("-------------");
+    let start = Instant::now();
+    let extrema = find_extrema(&a);
+    let elapsed = start.elapsed();
+    println!("extrema calculation: {:?}", elapsed);
+    println!("extrema is {:?}", extrema.least);
     println!("extrema is {:?}, {:p}", extrema, &extrema);
 
     println!("----------------");
     println!("-- With float --");
     println!("----------------");
-    let a = vec![0., 1., 10., -10., 100.];
-    let extrema = find_extremas(&a);
-    // extrema.least = &100;
-    println!("extrema is {:p}", extrema.least);
+    let start = Instant::now();
+    let extrema = find_extrema(&a_f);
+    let elapsed = start.elapsed();
+    println!("extrema calculation: {:?}", elapsed);
+    println!("extrema is {:?} {:p}", extrema.least, &extrema.least);
     println!("extrema is {:?}, {:p}", extrema, &extrema);
 }
